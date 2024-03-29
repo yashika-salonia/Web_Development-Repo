7c567954378a84e7bd33e1a33761774b96549bf7
yashika-salonia <yashikasalonia20@gmail.com>
2024-03-29 23:01:46 +0530

    while loop

---
+++ day_12/while_loop.js
@@ -0,0 +1,21 @@
+//While loop 
+let i=1
+while(i<10){
+    console.log(`Value of i: ${i}`);
+    i+=2;
+}
+
+//while loop on array
+let myArr=['blue','black','grey']
+let arr=0
+while(arr<myArr.length){
+    console.log(`Key: ${arr} , Value: ${myArr[arr]}`);
+    arr++;
+}
+
+//do while  loop
+let num=11
+do{
+    console.log(`Number: ${num}`);
+    num+=3
+} while(num<=10);
