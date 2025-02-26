f4106fb662d400110f473a981c9b17446c3a8fdd
yashika-salonia <yashikasalonia20@gmail.com>
2025-02-26 19:21:00 +0530

    added dom file

---
+++ date/date.js
@@ -0,0 +1,8 @@
+let mydate=new Date();
+console.log(mydate); //2025-02-26T06:30:01.932Z
+console.log(mydate.getTime()) //1740551401932
+console.log(mydate.getFullYear()) //2025
+console.log(mydate.getDay()); //3
+console.log(mydate.getHours()); //12
+console.log(mydate.getMinutes()); //0
+console.log(mydate.getMilliseconds()); //932
