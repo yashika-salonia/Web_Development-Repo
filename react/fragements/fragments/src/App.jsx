13fb2073605d92c11e48d2fa66c5f3e9a53b17db
yashika-salonia <yashikasalonia20@gmail.com>
2025-03-15 23:14:50 +0530

    using conditional rendering

--- react/fragements/fragments/src/App.jsx
+++ react/fragements/fragments/src/App.jsx
@@ -1,19 +1,39 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
+
+  let foodItems=["Dal", "Roti","Salad","Milk","Ghee"];
+  // let foodItems=[];
+
+  // conditional rendering
+  // Terninary operator 
+  let emptyMessage=foodItems.length === 0 ? <h3>No food items added.</h3>:null;
+
   return (
     // <div> - to avoid this div
     //1. use React.Fragment -<React.Fragment></React.Fragment>
     //2. <>
     <>
       <h1>Healthy Food</h1>
-      <ul class="list-group">
+
+      {/* truthy-falsy values - Logical operator */}
+      {/* {foodItems.length === 0 && <h3>No food items added.</h3>} */}
+
+      {emptyMessage}
+      <ul className="list-group">
+        {foodItems.map( (item) => (
+          <li key={item} className="list-group-item" >{item}</li>
+        ))}
+      </ul>
+
+      {/* Fragments */}
+      {/* <ul class="list-group">
         <li class="list-group-item" aria-current="true">Dal</li>
         <li class="list-group-item">Green Vegetables</li>
         <li class="list-group-item">Milk</li>
         <li class="list-group-item">Roti</li>
         <li class="list-group-item">Salad</li>
-      </ul>
+      </ul> */}
     </>
     // </div>
   );
